ae4e20810f97b43ce818790d62d2e14c9b21064e
Maxxiiime <max2002guillon@gmail.com>
2026-05-20 12:07:37 +0200

    refactor: - fix bidirectionnal loop between Leva & Zustand - create a new component for a better separation of responsibilities

---
+++ src/features/ThreeCanvas/components/RackSystem/ColumnAssembly.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Column, Arm, Leg } from './Parts';
+import { useShelfParts } from '@/hooks/useShelfParts';
+import { RackType } from '@/stores/rackStore';
+
+interface ColumnAssemblyProps {
+  columnId: string;
+  legId: string;
+  armId: string;
+  rackType: RackType;
+  numLevels: number;
+  position?: [number, number, number];
+}
+
+export const ColumnAssembly: React.FC<ColumnAssemblyProps> = ({
+  columnId,
+  legId,
+  armId,
+  rackType,
+  numLevels,
+  position = [0, 0, 0]
+}) => {
+  const { getColumnHeight, getOffsets } = useShelfParts();
+  
+  const offsets = getOffsets();
+  const levels = Array.from({ length: numLevels });
+  
+  const offsetArmBottom = offsets.arm_start_y;
+  const offsetArmTop = offsets.arm_end_y;
+  const columnHeightUnits = getColumnHeight(columnId);
+  const availableHeight = columnHeightUnits - offsetArmBottom - offsetArmTop;
+  const dynamicSpacingY = numLevels > 1 ? availableHeight / (numLevels - 1) : 0;
+
+  return (
+    <group position={position}>
+      <Column id={columnId} position={[offsets.column_x, 0, offsets.column_z]} />
+      <Leg 
+        id={legId} 
+        position={
+          rackType === 'double' 
+            ? [offsets.leg_x, offsets.leg_y, offsets.double_leg_z_offset]
+            : [offsets.leg_x, offsets.leg_y, offsets.leg_z]
+        } 
+      />
+      {levels.map((_, i) => {
+        const yPos = offsetArmBottom + i * dynamicSpacingY;
+        return (
+          <group key={`arm-${i}`}>
+            <Arm 
+              id={armId} 
+              position={[offsets.arm_x_offset, yPos, offsets.arm_z_offset]} 
+            />
+            {rackType === 'double' && (
+              <Arm 
+                id={armId} 
+                position={[offsets.double_arm_x_offset, yPos, offsets.double_arm_z_offset]} 
+                rotation={[0, Math.PI, 0]} // Rotate 180 degrees for double face
+              />
+            )}
+          </group>
+        );
+      })}
+    </group>
+  );
+};
--- src/features/ThreeCanvas/components/RackSystem/index.tsx
+++ src/features/ThreeCanvas/components/RackSystem/index.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useControls } from 'leva';
 import { useRackStore, RackType } from '@/stores/rackStore';
-import { Column, Arm, Leg } from './Parts';
+import { ColumnAssembly } from './ColumnAssembly';
 import { BraceAssembly } from './BraceAssembly';
 import { useShelfParts } from '@/hooks/useShelfParts';
 
 export const RackSystem: React.FC = () => {
-  const { 
-    rackType, 
-    numLevels, 
-    activeColumnId, 
-    activeArmId, 
-    activeBraceId, 
+  const {
+    rackType,
+    numLevels,
+    activeColumnId,
+    activeArmId,
+    activeBraceId,
     activeLegId,
     setRackType,
     setNumLevels,
@@ -20,32 +20,65 @@ export const RackSystem: React.FC = () => {
     setActiveBrace
   } = useRackStore();
 
-  const { getColumnsOptions, getArmsOptions, getBraceLength, getColumnHeight, getOffsets } = useShelfParts();
+  const { getColumnsOptions, getArmsOptions, getBraceLength } = useShelfParts();
 
-  // Setup Leva Controls
-  const columnsOpts = getColumnsOptions();
-  const armsOpts = getArmsOptions();
-  
+  // Setup Leva Controls with memoized options to prevent needless schema recreation
+  const columnsOpts = useMemo(() => getColumnsOptions(), []);
+  const armsOpts = useMemo(() => getArmsOptions(), []);
+
   const widthOpts = [750, 1000, 1250, 1500, 1750, 2000];
-  const initialWidth = Number(activeBraceId.split('_').pop() || 1000);
+  const initialWidth = useMemo(() => Number(activeBraceId.split('_').pop() || 1000), []);
 
-  const [controls, set] = useControls(() => ({
-    Type: { value: rackType, options: { 'Single': 'single', 'Double': 'double' } },
-    Levels: { value: numLevels, min: 1, max: 10, step: 1 },
-    Column: { value: activeColumnId, options: columnsOpts },
-    Arm: { value: activeArmId, options: armsOpts },
-    Width: { value: initialWidth, options: widthOpts },
-  }));
-
-  // Sync Leva -> Zustand
-  useEffect(() => {
-    if (controls.Type !== rackType) setRackType(controls.Type as RackType);
-    if (controls.Levels !== numLevels) setNumLevels(controls.Levels);
-    if (controls.Column !== activeColumnId) setActiveColumn(controls.Column);
-    if (controls.Arm !== activeArmId) setActiveArm(controls.Arm);
-    const newBraceId = `x_braces_${controls.Width}`;
-    if (newBraceId !== activeBraceId) setActiveBrace(newBraceId);
-  }, [controls, rackType, numLevels, activeColumnId, activeArmId, activeBraceId, setRackType, setNumLevels, setActiveColumn, setActiveArm, setActiveBrace]);
+  const [, set] = useControls(() => ({
+    Type: {
+      value: rackType,
+      options: { 'Single': 'single', 'Double': 'double' },
+      onChange: (v) => {
+        if (v !== rackType) {
+          setRackType(v as RackType);
+        }
+      }
+    },
+    Levels: {
+      value: numLevels,
+      min: 1,
+      max: 10,
+      step: 1,
+      onChange: (v) => {
+        if (v !== numLevels) {
+          setNumLevels(v);
+        }
+      }
+    },
+    Column: {
+      value: activeColumnId,
+      options: columnsOpts,
+      onChange: (v) => {
+        if (v !== activeColumnId) {
+          setActiveColumn(v);
+        }
+      }
+    },
+    Arm: {
+      value: activeArmId,
+      options: armsOpts,
+      onChange: (v) => {
+        if (v !== activeArmId) {
+          setActiveArm(v);
+        }
+      }
+    },
+    Width: {
+      value: initialWidth,
+      options: widthOpts,
+      onChange: (v) => {
+        const newBraceId = `x_braces_${v}`;
+        if (newBraceId !== activeBraceId) {
+          setActiveBrace(newBraceId);
+        }
+      }
+    },
+  }), [columnsOpts, armsOpts, initialWidth]);
 
   // Sync Zustand -> Leva (if changed externally)
   useEffect(() => {
@@ -63,80 +96,31 @@ export const RackSystem: React.FC = () => {
   const braceLengthKey = braceIdParts[braceIdParts.length - 1];
 
   const columnSpacing = getBraceLength(braceLengthKey);
-  const offsets = getOffsets();
-  
-  const levels = Array.from({ length: numLevels });
-  
-  const offsetArmBottom = offsets.arm_start_y;
-  const offsetArmTop = offsets.arm_end_y;
-  const columnHeightUnits = getColumnHeight(activeColumnId);
-  const availableHeight = columnHeightUnits - offsetArmBottom - offsetArmTop;
-  const dynamicSpacingY = numLevels > 1 ? availableHeight / (numLevels - 1) : 0;
 
   return (
     <group>
       {/* LEFT COLUMN */}
-      <group>
-        <Column id={activeColumnId} position={[offsets.column_x, 0, offsets.column_z]} />
-        <Leg 
-          id={activeLegId} 
-          position={
-            rackType === 'double' 
-              ? [offsets.leg_x, offsets.leg_y, offsets.double_leg_z_offset]
-              : [offsets.leg_x, offsets.leg_y, offsets.leg_z]
-          } 
-        />
-        {levels.map((_, i) => {
-          const yPos = offsetArmBottom + i * dynamicSpacingY;
-          return (
-          <group key={`left-arm-${i}`}>
-            <Arm 
-              id={activeArmId} 
-              position={[offsets.arm_x_offset, yPos, offsets.arm_z_offset]} 
-            />
-            {rackType === 'double' && (
-              <Arm 
-                id={activeArmId} 
-                position={[offsets.double_arm_x_offset, yPos, offsets.double_arm_z_offset]} 
-                rotation={[0, Math.PI, 0]} // Rotate 180 degrees for double face
-              />
-            )}
-          </group>
-        )})}
-      </group>
+      <ColumnAssembly
+        columnId={activeColumnId}
+        legId={activeLegId}
+        armId={activeArmId}
+        rackType={rackType}
+        numLevels={numLevels}
+      />
 
       {/* RIGHT COLUMN ASSEMBLY */}
-      <group position={[columnSpacing, 0, 0]}>
-        <Column id={activeColumnId} position={[offsets.column_x, 0, offsets.column_z]} />
-        <Leg 
-          id={activeLegId} 
-          position={
-            rackType === 'double' 
-              ? [offsets.leg_x, offsets.leg_y, offsets.double_leg_z_offset]
-              : [offsets.leg_x, offsets.leg_y, offsets.leg_z]
-          } 
-        />
-        {levels.map((_, i) => {
-          const yPos = offsetArmBottom + i * dynamicSpacingY;
-          return (
-          <group key={`right-arm-${i}`}>
-            <Arm 
-              id={activeArmId} 
-              position={[offsets.arm_x_offset, yPos, offsets.arm_z_offset]} 
-            />
-            {rackType === 'double' && (
-              <Arm 
-                id={activeArmId} 
-                position={[offsets.double_arm_x_offset, yPos, offsets.double_arm_z_offset]} 
-                rotation={[0, Math.PI, 0]} // Rotate 180 degrees for double face
-              />
-            )}
-          </group>
-        )})}
-      </group>
+      <ColumnAssembly
+        columnId={activeColumnId}
+        legId={activeLegId}
+        armId={activeArmId}
+        rackType={rackType}
+        numLevels={numLevels}
+        position={[columnSpacing, 0, 0]}
+      />
 
       {/* BRACE */}
       <BraceAssembly lengthKey={braceLengthKey} />
     </group>
   );
 };
+
