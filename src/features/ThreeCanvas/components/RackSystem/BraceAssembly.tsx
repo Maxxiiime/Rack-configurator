910136d2e204130285a3805ddd1870587e5aee5e
Maxxiiime <max2002guillon@gmail.com>
2026-05-21 15:04:15 +0200

    feat: improve brace display logic

--- src/features/ThreeCanvas/components/RackSystem/BraceAssembly.tsx
+++ src/features/ThreeCanvas/components/RackSystem/BraceAssembly.tsx
@@ -15,12 +15,22 @@ export const BraceAssembly: React.FC<BraceAssemblyProps> = ({ braceSize }) => {
   const xBraceId = findPartId('x_brace', braceSize) ?? '';
 
   const columnHeight = getColumnHeight(activeColumnId) - sizes.brace.top_offset;
+  const numLevels = Math.floor(columnHeight / sizes.brace.height);
 
   return (
     <group>
-      <BasePart id={hBraceId} position={[sizes.brace.h_x, columnHeight - sizes.brace.height, sizes.brace.h_z]} />
-      <BasePart id={xBraceId} position={[sizes.brace.x_x, columnHeight - sizes.brace.height, sizes.brace.x_z]} />
       <BasePart id={hBraceId} position={[sizes.brace.h_x, columnHeight, sizes.brace.h_z]} />
+      {Array.from({ length: numLevels }).map((_, i) => {
+        const y = columnHeight - (i + 1) * sizes.brace.height;
+        return (
+          <group key={i}>
+            <BasePart id={hBraceId} position={[sizes.brace.h_x, y, sizes.brace.h_z]} />
+            {i === 0 && (
+              <BasePart id={xBraceId} position={[sizes.brace.x_x, y, sizes.brace.x_z]} />
+            )}
+          </group>
+        );
+      })}
     </group>
   );
 };
