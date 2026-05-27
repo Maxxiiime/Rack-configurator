c644665370b493d091c727a1f07f04a1935661e5
Maxxiiime <max2002guillon@gmail.com>
2026-05-27 11:17:41 +0200

    fix: improve shadow to avoid truncate shadow

--- src/features/ThreeCanvas/components/Scene/index.tsx
+++ src/features/ThreeCanvas/components/Scene/index.tsx
@@ -4,12 +4,21 @@ import { RackSystem } from "../RackSystem";
 const Scene = () => {
     return (
         <>
-            <Stage 
-                environment={{ files: "/hdri/potsdamer_platz_1k.hdr" }}				
+            <Stage
+                environment={{ files: "/hdri/potsdamer_platz_1k.hdr" }}
                 adjustCamera={false}
+                intensity={1}
+                shadows={{
+                    type: 'contact',
+                    scale: 300,
+                    opacity: 1,
+                    blur: 0.25,
+                    resolution: 4096,
+                }}
             >
                 <RackSystem />
             </Stage>
+            <OrbitControls makeDefault />
             <OrbitControls makeDefault />
         </>
     );
