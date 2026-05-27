59e7b17e45deb09738fdb25e00b863a07b9f89ec
Maxxiiime <max2002guillon@gmail.com>
2026-05-27 16:33:40 +0200

    feat: edit camera default position

--- src/features/ThreeCanvas/index.tsx
+++ src/features/ThreeCanvas/index.tsx
@@ -6,7 +6,7 @@ import { Perf } from "r3f-perf";
 const ThreeCanvas = () => {
 	return (
 		<Box w="100%" h="100vh" zIndex={0}>
-			<Canvas camera={{ fov: 45, position: [0, 10, -30] }}>
+			<Canvas camera={{ fov: 45, position: [0, 5, -30] }}>
 				<Scene />
 				{import.meta.env.MODE === "development" && <Perf position="top-left" overClock />}
 			</Canvas>
